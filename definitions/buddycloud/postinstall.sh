3bebe54b936ca6e84e92ef8886d66453a8bc03e2
Rene Treffer <treffer@measite.de>
2012-02-21 21:27:47 +0100

    Move some magic back to the postinstall

--- definitions/buddycloud/postinstall.sh
+++ definitions/buddycloud/postinstall.sh
@@ -4,6 +4,11 @@ date > /etc/vagrant_box_build_time
 
 # update package list
 apt-get -y update
+apt-get -y dist-upgrade
+apt-get -y purge apparmor
+apt-get -y autoremove
+apt-get -y clean
+apt-get -y update
 
 # Setup sudo to allow no-password sudo for "admin"
 cp /etc/sudoers /etc/sudoers.orig
@@ -21,10 +26,6 @@ wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/key
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
-# Zero out the free space to save space in the final image:
-dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-
 # Removing leftover leases and persistent rules
 echo "cleaning up dhcp leases"
 rm /var/lib/dhcp3/*
@@ -40,3 +41,11 @@ rm /lib/udev/rules.d/75-persistent-net-generator.rules
 echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
 exit
+
+echo
+echo "Cleaning the disk"
+echo
+
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
