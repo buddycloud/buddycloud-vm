cdef31b38b5a2b5090556272ff430c9649f2c100
Rene Treffer <treffer@measite.de>
2012-03-03 13:14:16 +0100

    Add component config

--- modules/buddycloud/manifests/xmpp.pp
+++ modules/buddycloud/manifests/xmpp.pp
@@ -51,5 +51,9 @@ define buddycloud::xmpp::config(
 }
 
 define buddycloud::xmpp::component() {
+    file {'/etc/buddycloud/component-config.js':
+        ensure  => present,
+        content => template("puppet:///buddycloud/component-config.js"),
+    }
 }
 
