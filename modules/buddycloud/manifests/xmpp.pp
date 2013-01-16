a9fabdaf2036323d61e7a5864f2acb734a05d329
Denis Washington <denisw@online.de>
2013-01-16 16:33:05 +0100

    Use prosody from the offical Ubuntu repositories, it's current

--- modules/buddycloud/manifests/xmpp.pp
+++ modules/buddycloud/manifests/xmpp.pp
@@ -1,31 +1,11 @@
-class buddycloud::xmpp::repository {
-    include lua-dbi
-    apt::key {"prosody":
-        ensure => present,
-        source => 'http://prosody.im/files/prosody-debian-packages.key',
-    }
-    apt::key {"prosody-ppa":
-        ensure => present,
-        content => "-----BEGIN PGP PUBLIC KEY BLOCK-----\nVersion: SKS 1.0.10\n\nmI0ESejI7gEEAK3TQd9orDIBZYP7TpGNyfP1xhOK7ubQQS28YGXkQjKIYAQ67l9eNja3+P8t\nkfqstu9EPeGs8cwC0w+1vBLz/gDy12Yee3FA//lxNCJxER+DD5bL/N1YNJU5W/ePeOHhpk5N\naWmq/Tg2nM5NUf0TcW6SYqOzLILYWFx6HRKJ88KFABEBAAG0J0xhdW5jaHBhZCBQUEEgZm9y\nIFByb3NvZHkgSU0gRGV2ZWxvcGVyc4i2BBMBAgAgBQJJ6MjuAhsDBgsJCAcDAgQVAggDBBYC\nAwECHgECF4AACgkQqDGxck3uKwOjbAQAorqJ2x1KCkezsmJP7m7PPlBAaOiegp8TwSE8u/tA\nDvGMfP7ISoRX4lSkZLKNTkZ/1qs15CJrg3PE4gpNhGZh6q4Bj1n/DCAh3W+h5c082xcZXkCF\n2efgbSxJOvYeOdbY0il80SP8ql+nZ8oMiUMWyiE7wuKEPPD/2rK/ejxDdeA=\n=avQM\n-----END PGP PUBLIC KEY BLOCK-----",
-    }
-    apt::sources_list{"prosody":
-        ensure  => present,
-        content => "deb http://ppa.launchpad.net/prosody-dev/ppa/ubuntu lucid main\ndeb http://packages.prosody.im/debian maverick main",
-        require => [Apt::Key['prosody'],Apt::Key['prosody-ppa']],
-    }
-}
-
 class buddycloud::xmpp {
-    class{"buddycloud::xmpp::repository": stage => 'apt'}
-    package { "liblua5.1-sql-postgres-2": ensure => installed }
-    package { "lua-zlib": ensure => installed, require => Apt::Sources_list['prosody'] }
-    package{"libicu-dev": ensure => installed}
-    package{"build-essential": ensure => installed}
-    package{"node-stringprep":
+    include lua-dbi
+    package { "libicu-dev": ensure => installed}
+    package { "build-essential": ensure => installed}
+    package { "node-stringprep":
         provider => 'npm',
         require  => [Package['npm'], Package['libicu-dev'], Package['build-essential']],
     }
-
 }
 
 define buddycloud::xmpp::config(
