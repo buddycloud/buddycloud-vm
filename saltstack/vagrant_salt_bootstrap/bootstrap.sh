1a3c88b2f164fb8223e12b5b45e2fcb727e6253b
Simon Tennant <simon@buddycloud.com>
2015-09-03 18:40:00 +0200

    this is working out quite nicely

--- saltstack/vagrant_salt_bootstrap/bootstrap.sh
+++ saltstack/vagrant_salt_bootstrap/bootstrap.sh
@@ -5,7 +5,9 @@ apt-get --yes -q update
 apt-get --yes -q install python-git salt-master salt-minion
 cp /srv/vagrant_salt_bootstrap/master /etc/salt/master
 cp /srv/vagrant_salt_bootstrap/minion /etc/salt/minon 
+salt-key --gen-keys=buddycloud-vm
+cp buddycloud-vm.pub /etc/salt/pki/master/minions/buddycloud-vm.pub
 service salt-minion restart
 service salt-master restart
 sleep 10
-salt-key -A 
+salt -v "*" state.highstate -l debug
