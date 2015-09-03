0f47a03cde6d41e00a9e1832c076f0e228ab3556
Simon Tennant <simon@buddycloud.com>
2015-09-03 19:23:16 +0200

    still problematic

--- saltstack/vagrant_salt_bootstrap/bootstrap.sh
+++ saltstack/vagrant_salt_bootstrap/bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+export DEBIAN_FRONTEND=noninteractive
 apt-get --yes -q install python-software-properties
-add-apt-repository ppa:saltstack/salt2015-5
+add-apt-repository ppa:saltstack/salt2015-5 -y
 apt-get --yes -q update
 apt-get --yes -q install python-git salt-master salt-minion
+service salt-minion stop
+service salt-master stop
 cp /srv/vagrant_salt_bootstrap/master /etc/salt/master
 cp /srv/vagrant_salt_bootstrap/minion /etc/salt/minon 
 salt-key --gen-keys=buddycloud-vm
 cp buddycloud-vm.pub /etc/salt/pki/master/minions/buddycloud-vm.pub
-service salt-minion restart
-service salt-master restart
+service salt-minion start
+service salt-master start
 sleep 10
 salt -v "*" state.highstate -l debug
